f654b315a0674f4860b83c301539d515363dad27
Marco Boccia <pokermark95@live.it>
2022-02-16 01:17:37 +0100

    fatte piccole modifiche css al form inserisci annuncio

--- resources/views/announcement/announcement_form.blade.php
+++ resources/views/announcement/announcement_form.blade.php
@@ -9,53 +9,64 @@
              </ul>
          </div>
      @endif
-
-     <header class=" d-flex justify-content-center align-items-center ">
-         <form style="margin-top: 3rem; background-color:royalblue;" class="form" method="POST" action="{{ route('announcement_submit') }}" enctype="multipart/form-data">
-
-             @csrf
-
-             <input type="hidden" name="uniqueSecret" value="{{ $uniqueSecret }}" id="uniqueSecret">
-             <div class="mb-3">
-                 <h5 style="display:none"> {{ $uniqueSecret }}</h5>
-                 <label for="exampleInputEmail1" class="form-label text-white">Inserisci il titolo</label>
-                 <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                     name="title" value="{{old('title')}}">
-
-             </div>
-             <div class="mb-3">
-                 <label for="exampleInputPassword1" class="form-label text-white">Inserisci la descrizione</label>
-                 <input type="text" class="form-control" id="exampleInputPassword1" name="description" value="{{old('description')}}">
-             </div>
-             <div class="mb-3">
-                 <label for="exampleInputText" class="form-label text-white">Inserisci il prezzo</label>
-                 <input type="text" class="form-control" name="price" value="{{old('price')}}">
-             </div>
-             <div class="mb-3">
-                 <select class="form-select text-danger" name="category" id="category" >
-                     @foreach ($categories as $category)
-                         <option value="{{ $category->id }}">
-                             {{ $category->name }}
-                         </option>
-                     @endforeach
-                 </select>
-             </div>
-             {{-- DROPZONE --}}
-             <div class='form-group row'>
-                 <label form="images" class="col-md-12 col-form-label text-md-right">Immagini
-                 </label>
-                 <div class="col-md-12">
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <header class=" d-flex justify-content-center align-items-center mt-5">
+                <form class="form" method="POST" action="{{ route('announcement_submit') }}" enctype="multipart/form-data">
+       
+                    @csrf
+       
+                    <input type="hidden" name="uniqueSecret" value="{{ $uniqueSecret }}" id="uniqueSecret">
+                    <div class="mb-3">
+                        <h5 style="display:none"> {{ $uniqueSecret }}</h5>
+                        <label for="exampleInputEmail1" class="form-label text-white">Inserisci il titolo</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            name="title" value="{{old('title')}}">
+       
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label text-white">Inserisci la descrizione</label>
+                        <input type="text" class="form-control" id="exampleInputPassword1" name="description" value="{{old('description')}}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputText" class="form-label text-white">Inserisci il prezzo</label>
+                        <input type="text" class="form-control" name="price" value="{{old('price')}}">
+                    </div>
+                    <div class="mb-3">
+                        <select class="form-select text-danger" name="category" id="category" >
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">
+                                    {{ $category->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </header>
+            </form>
+        </div>
+        {{-- DROPZONE --}}
+        <div class="col-6">
+            <div class='form-group row'>
+                <label form="images" class="col-md-12 col-form-label text-md-right">Immagini
+                </label>
+                <div class="col-md-12">
+            
+                    <div class="dropzone" id="drophere"></div>
+            
+                    @error('images')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>({ $message )}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            <button type="submit" class="button-24">Submit</button>
+            
+        </div>
+    </div>
+</div>
      
-                     <div class="dropzone" id="drophere"></div>
-     
-                     @error('images')
-                         <span class="invalid-feedback" role="alert">
-                             <strong>({ $message )}</strong>
-                         </span>
-                     @enderror
-                 </div>
-             </div>
-             <button type="submit" class="button-24">Submit</button>
-            </header>
-        </form>
+
+    
         </x-layout>
